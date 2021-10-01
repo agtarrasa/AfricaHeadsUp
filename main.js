c2c97f2c385b80ff3e54da7bd482b2866ea67cac
agtarrasa <fastdeliveries2020@gmail.com>
2021-10-01 16:06:19 +0000

    added method that generates 3 random positions bettwen 0-2 to set choices

--- main.js
+++ main.js
@@ -140,6 +140,19 @@ function getFlags(){
                                 $('.correctNotCorrect').eq(1).text('');
                                 $('.correctNotCorrect').eq(2).text('');
                             }
+                            function zeroToRandom(){
+                                var i=0;
+                            var positions=[];
+                            var RandomNumber=0;
+                            while(positions.length!=3){
+                            RandomNumber=Math.floor(Math.random()*3);
+                            while(positions.includes(RandomNumber) ){
+                            RandomNumber=Math.floor(Math.random()*3);
+                            }
+                            positions.push(RandomNumber);
+                            }
+                            return positions;
+                            }
 
     var theFlags=getFlags(); 
     var thePositions= getRandomPositions();
